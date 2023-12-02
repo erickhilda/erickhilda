bbbb137f50281730cc3c4e280ecefa6e1c661f63
erickhilda <erickh.andreas@gmail.com>
2023-12-02 14:44:06 +0700

    use hero icon instead of svg

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,4 +1,5 @@
 import { useTheme } from 'next-themes'
+import { MoonIcon, SunIcon } from '@heroicons/react/24/outline'
 
 function Navbar() {
   const { setTheme, resolvedTheme } = useTheme()
@@ -9,42 +10,16 @@ function Navbar() {
       <div className='flex items-center'>
         <nav className='inline-flex gap-4 dark:text-white items-center capitalize font-semibold'>
           <button
-            className='p-2 focus:outline-none text-outer-space dark:text-gold'
+            className='p-2 focus:outline-none text-outer-space dark:text-white'
             title='Toggles light & dark'
             onClick={() =>
               setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')
             }
           >
             {resolvedTheme === 'dark' ? (
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                className='h-6 w-6'
-                fill='none'
-                viewBox='0 0 24 24'
-                stroke='currentColor'
-              >
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  strokeWidth={2}
-                  d='M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z'
-                />
-              </svg>
+              <MoonIcon className='h-6 w-6' />
             ) : (
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                className='h-6 w-6'
-                fill='none'
-                viewBox='0 0 24 24'
-                stroke='currentColor'
-              >
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  strokeWidth={2}
-                  d='M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z'
-                />
-              </svg>
+              <SunIcon className='h-6 w-6' />
             )}
           </button>
         </nav>
